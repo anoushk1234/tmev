2db1efe4e7077f2653704c999d79e79488ca8851
anoushk1234 <anoushka1828@gmail.com>
2023-01-16 14:07:27 +0530

    feat: trying to add threads

--- src/arb_table.rs
+++ src/arb_table.rs
@@ -1,15 +1,22 @@
+use crate::{arb_feed::*, get_and_parse_arb_feed};
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use spinners::{Spinner, Spinners};
+use std::sync::Arc;
 use std::{
     error::Error,
     io::{self, Stdout},
     os::unix::thread,
-    thread::sleep,
+    // thread::sleep,
     time::Duration,
 };
+use std::{marker::Send, vec};
+use tokio::sync::Mutex;
+use tokio::time::sleep;
+use tokio_util::task::LocalPoolHandle;
 use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Constraint, Layout},
@@ -18,15 +25,14 @@ use tui::{
     Frame, Terminal,
 };
 
-use crate::arb_feed::QueryData;
-
-pub struct App<'a> {
+pub struct App {
     state: TableState,
-    items: Vec<Vec<&'a str>>,
+    items: Vec<Vec<String>>,
 }
-
-impl<'a> App<'a> {
-    pub fn new(rows: Vec<Vec<&'a str>>) -> App<'a> {
+// unsafe impl Send for App {}
+// unsafe impl Sync for App {}
+impl App {
+    pub fn new(rows: Vec<Vec<String>>) -> App {
         App {
             state: TableState::default(),
             items: rows,
@@ -59,10 +65,18 @@ impl<'a> App<'a> {
         };
         self.state.select(Some(i));
     }
+    pub fn go_to_explorer(&mut self) {
+        let row_index = self.state.selected().unwrap();
+        let row = self.items.get(row_index).unwrap();
+        let mut explorer = "https://explorer.solana.com/tx/".to_string().to_owned();
+        explorer.push_str(row.get(2).unwrap());
+
+        open::that(explorer).unwrap();
+    }
 }
 
-pub fn display_table(
-    rows: Vec<Vec<&str>>,
+pub async fn display_table(
+    rows: Vec<Vec<String>>,
 ) -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
     // setup terminal
     enable_raw_mode()?;
@@ -73,7 +87,7 @@ pub fn display_table(
 
     // create app and run it
     let app = App::new(rows);
-    let res = run_app(&mut terminal, app);
+    let res = run_app(&mut terminal, app).await;
 
     // restore terminal
     disable_raw_mode()?;
@@ -87,31 +101,68 @@ pub fn display_table(
     if let Err(err) = res {
         println!("{:?}", err)
     }
-    // sleep(Duration::from_millis(4000));
-    // terminal.clear();
+
     Ok(terminal)
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+async fn run_app<B: Backend + std::marker::Send>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+) -> io::Result<()> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<Vec<Vec<String>>>(9000);
+    let local_set = LocalPoolHandle::new(1);
+    // let tx2 = tx.clone();
+    // println!("enter");
+    local_set.spawn_pinned(async move || loop {
+        // panic!("fuck");
+        sleep(Duration::from_millis(5000)).await;
+        // let mut sp = Spinner::new(Spinners::Dots8Bit, " updating".into());
+        let resp = get_and_parse_arb_feed().await.unwrap();
+        tx.send(resp).await.unwrap();
+        // sp.stop();
+    });
+    // let mut newitems: Vec<Vec<String>> = Vec::new();
+    // newitems = app.items.clone();
     Ok(loop {
+        // app.items = newitems.clone();
         terminal.draw(|f| ui(f, &mut app))?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Char('q') => break,
                 KeyCode::Down => app.next(),
                 KeyCode::Up => app.previous(),
                 KeyCode::Char('r') => {
                     terminal.clear();
-                    break;
+                    sleep(Duration::from_millis(500));
+                    continue;
                 }
-                _ => {}
+                KeyCode::Enter => app.go_to_explorer(),
+                _ => {
+                    if let Some(mut msg) = rx.recv().await {
+                        if msg.len() > 0 {
+                            // terminal.clear();
+                            // msg.push(vec![
+                            //     "hello1".to_string(),
+                            //     "2".to_string(),
+                            //     "3".to_string(),
+                            //     "4".to_string(),
+                            // ]);
+                            // newitems = vec![vec![String::from("something")]];
+                            continue;
+                            // app.state.select(index)
+                            // println!("reaches here");
+                        }
+                    }
+                    // continue;
+                }
             }
         }
     })
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    // println!("redraw :{:?}", app.items.len());
     let rects = Layout::default()
         .constraints([Constraint::Percentage(100)].as_ref())
         .margin(2)
@@ -142,7 +193,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
             .max()
             .unwrap_or(0)
             + 1;
-        let cells = item.iter().map(|c| Cell::from(*c));
+        let cells = item.iter().map(|c| Cell::from(c.clone()));
         Row::new(cells).height(height as u16).bottom_margin(1)
     });
     let t = Table::new(rows)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#![feature(async_closure)]
+use std::error::Error;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 use crate::arb_feed::ArbFeedResponse;
+use arb_feed::QueryData;
 use clap::arg;
 use clap::command;
 use clap::ArgMatches;
@@ -34,7 +38,9 @@ mod arb_feed;
 mod arb_table;
 
 use arb_table::*;
+use tokio::sync::Mutex;
 use tokio::time::sleep;
+
 #[tokio::main]
 async fn main() {
     let cmd = Command::new("tmev").args(&[
@@ -52,91 +58,147 @@ async fn main() {
             }
             "arbs" => {
                 // println!("{}", a);
-                loop {
-                    let mut sp = Spinner::new(Spinners::Dots8Bit, " loading arbs 🥩".into());
-                    let feed = arb_feed::get_arb_feed().await;
-                    let parsed = serde_json::from_str::<ArbFeedResponse>(
-                        feed.unwrap().text().await.unwrap().as_str(),
-                    )
-                    .unwrap()
-                    .query_data;
-                    let block_times = parsed
-                        .block_time
-                        .iter()
-                        .map(|f| f.as_str())
-                        .collect::<Vec<&str>>();
-                    let slot_ids = parsed
-                        .slot_id
-                        .iter()
-                        .map(|f| f.as_str())
-                        .collect::<Vec<&str>>();
-                    let transaction_hashes = parsed
-                        .transaction_hash
-                        .iter()
-                        .map(|f| f.as_str())
-                        .collect::<Vec<&str>>();
 
-                    let profit_amts_string = parsed
-                        .profit_amount
-                        .iter()
-                        .map(|a| a.to_string())
-                        .collect::<Vec<String>>();
-                    let profit_amts = profit_amts_string
-                        .iter()
-                        .map(|f| f.as_str())
-                        .collect::<Vec<&str>>();
-                    let currencies = parsed
-                        .currency
-                        .iter()
-                        .map(|c| c.as_str())
-                        .collect::<Vec<&str>>();
-                    let signers = parsed
-                        .signers
-                        .iter()
-                        .map(|f| f.as_str())
-                        .collect::<Vec<&str>>();
-                    let prices_usd = parsed
-                        .price_usd
-                        .iter()
-                        .map(|f| match f.as_str() {
-                            Some(f) => f,
-                            None => "None",
-                        })
-                        .collect::<Vec<&str>>();
-                    let profits_usd = parsed
-                        .price_usd
-                        .iter()
-                        .map(|f| match f.as_str() {
-                            Some(f) => f,
-                            None => "None",
-                        })
-                        .collect::<Vec<&str>>();
-                    let mut row_vec = Vec::new();
+                let mut sp = Spinner::new(Spinners::Dots8Bit, " loading arbs 🥩".into());
+                let feed = arb_feed::get_arb_feed().await;
+                let parsed = serde_json::from_str::<ArbFeedResponse>(
+                    feed.unwrap().text().await.unwrap().as_str(),
+                )
+                .unwrap()
+                .query_data;
+                // let block_times = parsed
+                //     .block_time
+                //     .iter()
+                //     .map(|f| f.as_str())
+                //     .collect::<Vec<&str>>();
+                // let slot_ids = parsed
+                //     .slot_id
+                //     .iter()
+                //     .map(|f| f.as_str())
+                //     .collect::<Vec<&str>>();
+                // let transaction_hashes = parsed
+                //     .transaction_hash
+                //     .iter()
+                //     .map(|f| f.as_str())
+                //     .collect::<Vec<&str>>();
 
-                    for index in 0..transaction_hashes.len() {
-                        let row = vec![
-                            block_times[index],
-                            slot_ids[index],
-                            transaction_hashes[index],
-                            profit_amts[index],
-                            currencies[index],
-                            signers[index],
-                            prices_usd[index],
-                            profit_amts[index],
-                        ];
-                        row_vec.push(row);
-                    }
+                let profit_amts = parsed
+                    .profit_amount
+                    .iter()
+                    .map(|a| a.to_string())
+                    .collect::<Vec<String>>();
+                // let profit_amts = profit_amts_string
+                //     .iter()
+                //     .map(|f| f.as_str())
+                //     .collect::<Vec<&str>>();
+                // let currencies = parsed
+                //     .currency
+                //     .iter()
+                //     .map(|c| c.as_str())
+                //     .collect::<Vec<&str>>();
+                // let signers = parsed
+                //     .signers
+                //     .iter()
+                //     .map(|f| f.as_str())
+                //     .collect::<Vec<&str>>();
+                let prices_usd = parsed
+                    .price_usd
+                    .iter()
+                    .map(|f| f.to_string())
+                    .collect::<Vec<String>>();
+                let profits_usd = parsed
+                    .price_usd
+                    .iter()
+                    .map(|f| f.to_string())
+                    .collect::<Vec<String>>();
+                let QueryData {
+                    block_time,
+                    slot_id,
+                    transaction_hash,
+                    profit_amount,
+                    currency,
+                    signers,
+                    price_usd,
+                    profit_usd,
+                } = parsed;
+                let mut row_vec = Vec::new();
 
-                    // sleep(Duration::from_secs(3));
-                    sp.stop();
-
-                    let mut terminal = display_table(row_vec).unwrap();
-                    // thread::sleep(Duration::from_millis(5000));
+                for index in 0..transaction_hash.len() {
+                    let row: Vec<String> = vec![
+                        block_time[index].clone(),
+                        slot_id[index].clone(),
+                        transaction_hash[index].clone(),
+                        profit_amts[index].clone(),
+                        currency[index].clone(),
+                        signers[index].clone(),
+                        prices_usd[index].clone(),
+                        profits_usd[index].clone(),
+                    ];
+                    row_vec.push(row);
                 }
+
+                sp.stop();
+
+                display_table(row_vec).await.unwrap();
             }
+
             _ => {
                 // overview ui
             }
         }
     }
+}
+
+// type Something = Arc<tokio::sync::Mutex<Vec<Vec<String>>>>;
+// impl Something for SomethingStruct {}
+// type Res<T> = Result<T, dyn Error>;
+pub async fn get_and_parse_arb_feed() -> Result<Vec<Vec<String>>, Box<dyn Error + std::marker::Send>>
+{
+    let feed = arb_feed::get_arb_feed().await;
+    let parsed =
+        serde_json::from_str::<ArbFeedResponse>(feed.unwrap().text().await.unwrap().as_str())
+            .unwrap()
+            .query_data;
+
+    let profit_amts = parsed
+        .profit_amount
+        .iter()
+        .map(|a| a.to_string())
+        .collect::<Vec<String>>();
+    let prices_usd = parsed
+        .price_usd
+        .iter()
+        .map(|f| f.to_string())
+        .collect::<Vec<String>>();
+    let profits_usd = parsed
+        .price_usd
+        .iter()
+        .map(|f| f.to_string())
+        .collect::<Vec<String>>();
+    let QueryData {
+        block_time,
+        slot_id,
+        transaction_hash,
+        profit_amount,
+        currency,
+        signers,
+        price_usd,
+        profit_usd,
+    } = parsed;
+    let mut row_vec = Vec::new();
+
+    for index in 0..transaction_hash.len() {
+        let row: Vec<String> = vec![
+            block_time[index].clone(),
+            slot_id[index].clone(),
+            transaction_hash[index].clone(),
+            profit_amts[index].clone(),
+            currency[index].clone(),
+            signers[index].clone(),
+            prices_usd[index].clone(),
+            profits_usd[index].clone(),
+        ];
+        row_vec.push(row);
+    }
+    Ok(row_vec)
 }
