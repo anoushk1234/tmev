bcb30a9decc5d0d6674e80285a26d474c941bbf6
anoushk1234 <anoushka1828@gmail.com>
2023-01-23 11:03:52 +0530

    diagnose shared state issue
    Co-authored-by: harsh4786 <harsh4786@users.noreply.github.com>

--- searcher-cli/send.sh
+++ searcher-cli/send.sh
@@ -1,5 +1,5 @@
 ../target/release/jito-searcher-cli \
-  --block-engine-url https://frankfurt.mainnet.block-engine.jito.wtf \
+  --block-engine-url https://amsterdam.mainnet.block-engine.jito.wtf \
   --keypair-path ../id.json \
   send-bundle \
   --payer ../id.json \
@@ -7,4 +7,4 @@
   --num-txs 1 \
   --lamports 100 \
   --tip-account 96gYZGLnJYVFmbjzopPSU6QiEV5fGqZNyN9nmNhvrZU5 \
-  --rpc-url "https://frankfurt.mainnet.rpc.jito.wtf/?access-token=76819d17-b796-4253-b7f5-6c543fb3c508"
+  --rpc-url "https://amsterdam.mainnet.rpc.jito.wtf/?access-token=76819d17-b796-4253-b7f5-6c543fb3c508"
